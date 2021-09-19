db8f0c8b0053f028a442a85487b70c14a4285bac
Rossi, Joe <joe_rossi@bose.com>
2021-09-19 07:43:57 -0400

    Adding include_absolute plugin.

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "Securing the Internet of Things (IoT)",
-        "excerpt":"The Rise of IoT Over the past decade, the Internet of Things (IoT) has transformed from a start-up buzzword into becoming an integral part of everyday life. The ubiquity and low cost of smartphones and the cloud have significantly lowered the bar to entry for new and existing businesses to...","categories": ["iot"],
+        "excerpt":"The Rise of IoT Over the past decade, the Internet of Things (IoT) has transformed from a start-up buzzword into becoming an integral part of everyday life. The ubiquity and low cost of smartphones and the cloud have significantly lowered the bar to entry for new and existing businesses to...","categories": [],
         "tags": [],
-        "url": "/EC601-IoT-Security/iot/2021/09/18/iot-security.html",
+        "url": "/EC601-IoT-Security/2021/09/18/iot-security.html",
         "teaser": null
       }]
