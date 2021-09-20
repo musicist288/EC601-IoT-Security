3691575595c622b1456ca09adfcce6c1dba53ac0
Rossi, Joe <joe_rossi@bose.com>
2021-09-19 20:30:54 -0400

    Pre-push site build.

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Securing the Internet of Things (IoT)",
-        "excerpt":"The Rise of IoT Over the past decade, the Internet of Things (IoT) has gone from being a start-up buzzword into becoming an integral part of everyday life. The ubiquity of smartphones and relatively low cost of cloud infrastructure have significantly lowered the bar to entry for new and existing...","categories": [],
+        "excerpt":"The rise of IoT Over the past decade, the Internet of Things (IoT) has gone from a start-up buzzword to an integral part of everyday life. The ubiquity of smartphones and relatively low cost of cloud infrastructure have significantly lowered the bar to entry for new and existing businesses to...","categories": [],
         "tags": [],
         "url": "/EC601-IoT-Security/2021/09/19/iot-security.html",
         "teaser": null
