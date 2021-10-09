8ac56da19bf094824684d73e0ed5f6fe396a5c6c
Joseph Rossi <musicist288@gmail.com>
2021-10-08 20:47:21 -0400

    Pre-push site build.

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/EC601-IoT-Security/2021/09/19/iot-security.html",
         "teaser": null
+      },{
+        "title": "Classifying Twitter Users",
+        "excerpt":"A Re-introduction to Twitter Twitter has changed a lot since I last used it. Over the years I’ve poked my head in occasionally, but I never really integrated Twitter into my everyday social media usage, so I had a lot to catch up on. It seems like, while hashtags are...","categories": [],
+        "tags": [],
+        "url": "/EC601-IoT-Security/2021/10/08/twitter-classifications.html",
+        "teaser": null
       }]
