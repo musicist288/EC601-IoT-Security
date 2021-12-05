38743ce11cf26b65c747231e9854478ecd84a8c7
Joseph Rossi <musicist288@gmail.com>
2021-12-05 11:06:21 -0500

    Pre-push site build.

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -11,7 +11,7 @@ var store = [{
         "url": "/EC601-IoT-Security/2021/10/08/twitter-classifications.html",
         "teaser": null
       },{
-        "title": "Domestic Violence in the IoT Age",
+        "title": "IoT Enabled Domestic Violence",
         "excerpt":"For the past five years of my career building IoT products and services, I have experienced firsthand the growing demand for building private and secure products. Early on, product definitions were scarce in security and privacy requirements. If any such requirements were specified, their focus was on protecting intellectual property,...","categories": [],
         "tags": [],
         "url": "/EC601-IoT-Security/2021/11/20/iot-societal-impact.html",
